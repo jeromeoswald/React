f0f73d5cf4294742a7235e2de51de51631132a15
jeromeoswald <jeromeoswald7@gmail.com>
2024-05-13 20:48:42 +0530

    React - state

--- react-list/src/App.jsx
+++ react-list/src/App.jsx
@@ -1,21 +1,26 @@
-import Post from './Post'
+import Post from "./Post"
 import './App.css'
-
 
 
 function App() {
 
-  let postone = "Jerome Oswald"
-  let posttwo = "Jack"
+  let postOne = "Manikandan's BD";
+  let postTwo = "Annual Day";
+  let postThree = "Farewell Party";
+
+  const headerStyle = {
+    textAlign: 'center', padding: 20, background: "#000", color: "#fff", textTransform: 'uppercase', margin: 10
+  }
 
   return (
-   <div>
-    <h1>React Components!!</h1>
-    <Post name={postone} venue = {"Chennai"}></Post>
-    <Post name={posttwo} venue = {"Trichy"}></Post>
-    <Post name={"Mavrick"} venue = {"USA"}></Post>
-
-   </div>
+    <section id="pricing">
+      <div style={headerStyle}> Price Card</div>
+      <div class="row">
+        <Post name="FREE" listing="" className="pricing-column col-lg-4 col-md-6"></Post>
+        <Post name="PLUS" listing="" className="pricing-column col-lg-4 col-md-6"></Post>
+        <Post name="PRO" listing="" className="pricing-column col-lg-4"></Post>
+      </div>
+    </section >
   )
 }
 
