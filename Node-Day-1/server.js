1280731123fc8b3b133f1e57c8d21f94ddcf58ca
Jerome Oswald Ebenezer J <jeromeoswald7@gmail.com>
2024-05-29 22:58:12 +0530

    Node-2

--- Node-Day-1/server.js
+++ Node-Day-1/server.js
@@ -1,50 +1,53 @@
 const express = require("express");
-// used to parse JSON Payloads in incoming request,
-//make it available in req.body
-const bodyParser = require ("body-parser");
-// file operation, create, read folder, file/folder exists or not
-const fs = require ("fs-extra");
-// to manage file & directory paths
-const path = require ("path");
-// const { log } = require("console");
+// used to parse JSON payloads in incoming request,
+// make it available in req.body
+const bodyParser = require("body-parser");
+// file operations, create, read folder, folder/file exists or not
+const fs = require("fs-extra");
+// manage file and directory paths
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
-const folderPath = path.join(__dirname,"texts");
+const folderPath = path.join(__dirname, "texts");
 
-// check whether the folder is there or not
-if(!fs.existsSync(folderPath)){
-    fs.mkdirSync(folderPath);
+// Check whether the folder is there or not
+if (!fs.existsSync(folderPath)) {
+  fs.mkdirSync(folderPath);
 }
 
-//Enpoint to create a text file
-app.post("/createFile",async (req, res)=>{
-try {
-    await fs.ensureDir(folderPath)
-    const time =new Date().toISOString();
+//Fucntion to remove the colon from filename
+function getFormattedFileName() {
+  return new Date().toISOString().replace(/:/g, "-");
+}
+
+// Endpoint to create a text file
+app.post("/createFile", async (req, res) => {
+  try {
+    await fs.ensureDir(folderPath);
+    const time = getFormattedFileName();
     const fileName = `${time}.txt`;
     const filePath = path.join(folderPath, fileName);
 
-    await fs.writeFile(filePath, time)
-    res.send("File Created successfully")
-} catch (error) {
-    console.log(error);
-    res.status(500).send("Error writing a file");
-}
-    
+    await fs.writeFile(filePath, time);
+    res.send("File created successfully!");
+  } catch (error) {
+    res.status(500).send("Error writing a file - ", error);
+  }
 });
 
+// Endpoint to retrieve all text files
+app.get("/getFiles", async (req, res) => {
+  try {
+    await fs.ensureDir(folderPath);
+    const files = await fs.readdir(folderPath);
+    const textFiles = files.filter((file) => file.endsWith(".txt"));
+    res.json(textFiles);
+  } catch (error) {
+    res.status(500).send("Error reading folder - ", error);
+  }
+});
 
-// app.get("/home",(req, res)=>{
-//     console.log("HELLO");
-//     res.send("Welcome to React Part One!!")
-// });
-
-// app.get("/",(req, res)=>{
-//     console.log("HI");
-//     res.send("Welcome to Node.js Part One!!")
-// });
-
-app.listen (PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
---
+++ User-Management-Full-Stack/User-Management-Full-Stack/back-end/routes/userRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const User = require("../models/User");
+
+const router = express.Router();
+
+//Crear a new user
+router.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//Crear a new user
+router.get("/users", async (req, res) => {
+  try {
+    const user = await User.find({});
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+module.exports = router;
