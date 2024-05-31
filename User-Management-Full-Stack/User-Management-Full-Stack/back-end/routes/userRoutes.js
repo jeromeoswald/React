261a2c0525651fbe877f12a148f3ced61f184868
Jerome Oswald Ebenezer J <jeromeoswald7@gmail.com>
2024-05-31 22:15:33 +0530

    bcrypt, authentication

--- User-Management-Full-Stack/User-Management-Full-Stack/back-end/routes/userRoutes.js
+++ User-Management-Full-Stack/User-Management-Full-Stack/back-end/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
@@ -57,6 +58,42 @@ router.delete("/users/:id", async (req, res) => {
     res.status(201).send(user);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+//Register User
+router.post("/register", async(req, res) => {
+  const {name, email, password} = req.body;
+  try{
+    const existingUser = await User.findOne({email});
+    if (existingUser){
+      return res.status(400).json({message: "User already exists."})
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword});
+    await newUser.save();
+    res.status(201).send ("User Resgistered Successfully")
+  } catch (error){
+    res.status(500).send("server error", error);
+  }
+});
+
+
+//login User
+router.post("/login", async(req, res) => {
+  const { email, password} = req.body;
+  try{
+    const user = await User.findOne({email});
+    if (!user){
+      return res.status(400).send("Invalid Username")
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch){
+      return res.status(400).send("Invalid Password");
+    }
+    res.status(201).send("Logged in Successfully!!");
+  } catch (error){
+    res.status(500).send("server error", error);
   }
 });
 
