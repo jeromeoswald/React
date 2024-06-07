b9379d30fb732f2569ef3a3c629bc4d97d0964d8
Jerome Oswald Ebenezer J <jeromeoswald7@gmail.com>
2024-06-07 13:17:19 +0530

    url-shortnere

--- MERN-Stack-URL-Shortener-main/server/server.js
+++ MERN-Stack-URL-Shortener-main/server/server.js
@@ -1,7 +1,7 @@
 const connect = require('./src/configs/db');
 const app = require('./src/index');
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, async () => {
